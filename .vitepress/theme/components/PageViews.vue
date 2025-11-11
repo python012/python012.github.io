6f44828c1fca9c38f5f0cf8d2d7523e59f7ba407
Reed Xia <huaqin2005@gmail.com>
2025-11-11 14:53:38 +0800

    add page view counter with busuanzi

---
+++ .vitepress/theme/components/PageViews.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-views">
+    <span id="busuanzi_container_page_pv">
+      👁️ 阅读量: <span id="busuanzi_value_page_pv"></span>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.page-views {
+  margin: 1rem 0;
+  padding: 0.5rem 0;
+  color: var(--vp-c-text-2);
+  font-size: 0.9rem;
+  border-top: 1px solid var(--vp-c-divider);
+}
+
+.page-views span {
+  color: var(--vp-c-brand-1);
+}
+</style>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,10 +1,27 @@
 import DefaultTheme from 'vitepress/theme'
 import ArticleMetadata from './components/ArticleMetadata.vue'
+import PageViews from './components/PageViews.vue'
 import './custom.css'
+import { onMounted, h } from 'vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('ArticleMetadata', ArticleMetadata)
+    app.component('PageViews', PageViews)
+  },
+  setup() {
+    onMounted(() => {
+      // Load busuanzi script for page view counter
+      const script = document.createElement('script')
+      script.async = true
+      script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
+      document.body.appendChild(script)
+    })
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-after': () => h(PageViews)
+    })
   }
 }
